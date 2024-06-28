36dcb4661c912f593ea3b8fc38271d6bd2377a6e
zakzakzak <zakiyff@gmail.com>
2024-06-28 21:19:45 +0700

    training ke 69

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def first_api():
-    return {"message" : "hello world!"}
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
