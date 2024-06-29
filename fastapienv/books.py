f7b9e5f16a6cf28f3f652a9adb6e76a9619b2c78
zakzakzak <zakiyff@gmail.com>
2024-06-29 15:00:34 +0700

    79 - Put Request

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -47,3 +47,8 @@ async def create_book(new_book=Body()):
     BOOKS.append(new_book)
 
 
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)): 
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
