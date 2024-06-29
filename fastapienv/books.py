a5270bb8e7df35c861d34195094191899b215730
zakzakzak <zakiyff@gmail.com>
2024-06-29 08:56:31 +0700

    71 - Path Parameter

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -14,3 +14,10 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+# Path Parameters
+@app.get("/books/{book_title}")
+async def read_all_books(book_title : str):
+    for book in BOOKS : 
+        if book.get('title').casefold() == book_title.casefold():
+            return book
