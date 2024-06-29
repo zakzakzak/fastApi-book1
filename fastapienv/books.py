5f3788324e84100f7ed1ce1a46b47c3d90074e26
zakzakzak <zakiyff@gmail.com>
2024-06-29 14:05:54 +0700

    75 - Post Request

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -40,5 +40,10 @@ async def read_author_category_by_query(book_author : str, category : str):
         book.get('category') == category.casefold() :
             books_to_return.append(book)
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
 
 
