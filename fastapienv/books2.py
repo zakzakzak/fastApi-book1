bd4d94398590bccbe04c12db317ed17e3c1ac9c5
zakzakzak <zakiyff@gmail.com>
2024-06-30 19:33:44 +0700

    92 - Update Book

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -79,3 +79,9 @@ async def create_book(book_request:BookRequest):
 def find_book_id(book:Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+@app.put("/books/update_book")
+async def update_book(book : BookRequest):
+    for i in range(len(BOOKS)) : 
+        if BOOKS[i].id == book.id :
+            BOOKS[i] = book
