df1bb9f84dd3572625cef8417b44ee6d1d51ed00
zakzakzak <zakiyff@gmail.com>
2024-06-30 13:10:17 +0700

    89 - Pydantic Configurations

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -28,6 +28,16 @@ class BookRequest(BaseModel):
     rating: int = Field(gt=-1, lt=6)
     published_date : int
 
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'A new book',
+                'author': 'codingwithroby',
+                'description': 'A new description of a book',
+                'rating': 5
+            }
+        } 
+
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
