568e797bb1c33a7b8cb22410d53c26d775479303
zakzakzak <zakiyff@gmail.com>
2024-06-30 19:25:30 +0700

    91 - Fetch Book by Rating

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -61,6 +61,15 @@ async def read_book(book_id : int):
         if book.id == book_id :
             return book
 
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = [] 
+    for book in BOOKS : 
+        if book.rating == book_rating :
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request:BookRequest):
     new_book = Book(**book_request.model_dump())
