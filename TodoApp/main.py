670335ad3f3448ea5ad219e4273c3c4df6c85c60
zakzakzak <zakiyff@gmail.com>
2024-07-05 23:39:54 +0700

    114 - Put Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -47,3 +47,19 @@ async def create_todo(db: db_dependency, todo_request : TodoRequest):
 
     db.add(todo_model)
     db.commit()
+
+@app.post("/todo/{todo_id}", status_code = status.HTTP_200_OK)
+async def update_todo(db : db_dependency, 
+                      todo_request : TodoRequest,
+                      todo_id : int = Path(gt=0) ):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None : 
+        raise HTTPException(status_code = 404, detail = 'Todo not found.')
+    
+    todo_model.title = todo_request.title 
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete 
+    
+    db.add(todo_model)
+    db.commit()
