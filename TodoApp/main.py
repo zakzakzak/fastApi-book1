2a84018358f1bb817511f0526e08175d6341c8a9
zakzakzak <zakiyff@gmail.com>
2024-07-05 23:13:16 +0700

    113 - Post Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from starlette import status
@@ -20,6 +21,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title       : str = Field(min_length = 3)
+    description : str = Field(min_length = 3, max_length = 100)
+    priority    : int = Field(gt=0, lt=6)
+    complete    : bool 
+
+
+
 @app.get("/", status_code = status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     # Depends() -> Dependency Injection : Menjalankan function get_db saat mulai, dan selesai (finally)
@@ -31,3 +40,10 @@ async def read_todo(db: db_dependency, todo_id:int= Path(gt=0)):
     if todo_model is not None : 
         return todo_model
     raise HTTPException(status_code = 404, detail='Todo not found.')
+
+@app.post("/todo", status_code = status.HTTP_200_OK)
+async def create_todo(db: db_dependency, todo_request : TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit()
