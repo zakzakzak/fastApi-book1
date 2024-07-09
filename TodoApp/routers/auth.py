f594940bed56224d0b02c8cd1a429923ec02f79b
zakzakzak <zakiyff@gmail.com>
2024-07-09 21:35:08 +0700

    122 - Create First User

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user' : 'authenticated'}
+class CreateUserRequest(BaseModel):
+    username   : str
+    email      : str
+    first_name : str
+    last_name  : str 
+    password   : str
+    role       : str
+
+@router.post("/auth")
+async def create_user(create_user_request : CreateUserRequest):
+    create_user_model = create_user_request
+    return create_user_model
