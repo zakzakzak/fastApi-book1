ad64507c0f74f98275a8e0de65c2dfdd50924309
zakzakzak <zakiyff@gmail.com>
2024-07-11 22:49:19 +0700

    128 - Decode JSON (revision)

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -54,16 +54,18 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     encode.update({'exp' : expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
+
 async def get_current_user(token : Annotated[str, Depends(oauth2_bearer)]):
     try :
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username : str = payload.get('sub')
+        username : str = payload.get('sub') 
         user_id : int = payload.get('id')
         if username is None or user_id is None :
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not Validate User.')
         return {'username':username, 'id': user_id}
+    except :
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not Validate User.')
     
-
 
 @router.post("/auth", status_code = status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
