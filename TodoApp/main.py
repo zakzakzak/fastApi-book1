4c07d367954e5011a4f9f23b31d965aafb7cdc71
zakzakzak <zakiyff@gmail.com>
2024-07-14 22:48:29 +0700

    assignment on progress...

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 import models
-from routers import auth, todos, admin
+from routers import auth, todos, admin, user
 
 app = FastAPI()
 
@@ -11,3 +11,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(user.router)
---
+++ TodoApp/routers/user.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
+from database import Sessionlocal
+from models import Todos
+from .auth import get_current_user 
+
+
+router = APIRouter(
+    prefix = '/user',
+    tags = ['user']
+)
+
+# # Hanya akan jalan ketika belum ada table nya
+# models.Base.metadata.create_all(bind=engine)
+# app.include_router(auth.router)
+
+def get_db():
+    db = Sessionlocal()
+    try : 
+        yield db
+    finally :
+        db.close()
+
+# Depends() -> Dependency Injection : Menjalankan function get_db saat mulai, dan selesai (finally)
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.get("/", status_code = status.HTTP_200_OK)
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user.get('role') != 'admin' : 
+        raise   HTTPException(status_code=401, detail='Unauthorized')
+    return db.query(Todos).all()
+
